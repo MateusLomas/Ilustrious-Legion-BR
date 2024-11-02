c32e13fe352125d942fa481c8c4551b458a8bdf4
Mateus Lomas <lomasdebora33@gmail.com>
2024-11-02 19:27:24 -0300

    2/11/2024

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ trilho.addEventListener('click', ()=> {
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
 })
+
+/* Criando Tela Carregando By: Atualizar Pagina */
+let elem_preloader = document.getElementById("preloader");
+let elem_loader = document.getElementById("loader");
+console.log("Testing... Ok");
+
+
+setTimeout(function() {
+  elem_preloader.classList.remove("preloader");
+  elem_loader.classList.remove("loader");
+  }, 1280);
